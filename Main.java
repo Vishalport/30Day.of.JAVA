b766be046fa1062526b4f9ae6f91df065d10f714
Vishal kumar <68020188+Vishalport@users.noreply.github.com>
2022-02-24 13:32:45 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,32 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Today: 24-02-2022/09:20");
-	// write your code here
+    public static void main(String[] arr) {
+        System.out.println("\t\tWelcome to the Test result.");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("\t\tEnter the student name : ");
+        String name = input.nextLine();
+
+        System.out.println("\t\tEnter the mark's of math : ");
+        int a = input.nextInt();
+
+        System.out.println("\t\tEnter the mark's of physcs : ");
+        int b = input.nextInt();
+
+        System.out.println("\t\tEnter the mark;s of chemistry : ");
+        int c = input.nextInt();
+
+        System.out.println("\t\tCongratulation your have input your student mark's ");
+
+        int cal = a + b + c;
+
+        System.out.print("name : ");
+        System.out.println(name);
+
+        System.out.print("total number in test is  : ");
+        System.out.println(cal);
     }
 }
