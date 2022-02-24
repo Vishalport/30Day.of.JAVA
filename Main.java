4dbe5d4f1041f23ccb089a71152ea9d6084ee940
Vishal kumar <68020188+Vishalport@users.noreply.github.com>
2022-02-24 13:32:12 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Today: 24-02-2022/09:20");
+	// write your code here
+    }
+}
